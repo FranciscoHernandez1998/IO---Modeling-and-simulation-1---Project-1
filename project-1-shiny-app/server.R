2ccdb200e77c9373550612f89f1477f14cf5d045
Francisco-H-1998 <francisco@sharegro.com>
2022-09-04 15:18:16 -0600

    chore(main): Improved UX/UI of first exercise

--- project-1-shiny-app/server.R
+++ project-1-shiny-app/server.R
@@ -5,4 +5,7 @@ shinyServer(function(input, output) {
     output$averages <- renderTable({
         get_week_days_average_queues(num_of_servers = input$num_of_servers)
     })
+    output$arrival_time_probabilities <- renderImage({
+     list(src = 'arrival-time-probabilities.JPG', width = 600)
+    }, deleteFile=FALSE)
 })
--- project-1-shiny-app/ui.R
+++ project-1-shiny-app/ui.R
@@ -7,12 +7,23 @@ shinyUI(fluidPage(
     navbarPage(title='Project 1',
         tabPanel('Ex. 1 - Customer Queue Average',
                  fluidRow(
+                   helpText('The next table displays the average size of the customers queue by day of the week 
+                            based on the number of servers available at the bank')
+                 ),
+                 fluidRow(
                      selectInput(
-                         "num_of_servers", "Select a number of servers", num_of_servers_options, selected = 7
+                         'num_of_servers', 'Select a number of servers', num_of_servers_options, selected = 7
                      )
                  ),
                  fluidRow(
-                     withSpinner(tableOutput("averages"))
+                     withSpinner(tableOutput('averages'))
+                 ),
+                 fluidRow(
+                   helpText('It takes and average time of 8 minutes for an agent to serve a customer and it has a standard deviation of 5 minutes'),
+                   helpText('The time between arrivals by day is given by the next table: ')
+                 ),
+                 fluidRow(
+                   withSpinner(imageOutput("arrival_time_probabilities"))
                  )
         )
     )
